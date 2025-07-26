852474b07725089a106ad58fde1af0aafbe22ad3
Steve Cirelli <scirelli+git@gmail.com>
2025-07-25 20:12:28 -0400

    Learning

---
+++ hello_cargo/target/debug/deps/hello_cargo-0802c6ae39de038f.d
@@ -0,0 +1,5 @@
+/home/scirelli/Projects/Steve/rust-hello_world/hello_cargo/target/debug/deps/hello_cargo-0802c6ae39de038f.d: src/main.rs
+
+/home/scirelli/Projects/Steve/rust-hello_world/hello_cargo/target/debug/deps/hello_cargo-0802c6ae39de038f: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/scirelli/Projects/Steve/rust-hello_world/hello_cargo/target/debug/hello_cargo: /home/scirelli/Projects/Steve/rust-hello_world/hello_cargo/src/main.rs
